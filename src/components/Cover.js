52ac9dca37a4ebde4a21fe6a3b08e4c1321a6355
Melvin Mbae <melvinmuthomi95@gmail.com>
2023-09-13 11:05:49 +0300

    components created and cover head rendered

---
+++ src/components/Cover.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Cover() {
+    return (
+        <div className="homepage">
+            <div className="cover">
+                <div className="title">
+                    <div className="heading">The Music Bar
+                        <img className="image" src="./music.png"></img>
+                    </div>
+                    <div class="tagline">A Music community where you can review your best albums and follow your favorite artists</div>
+                </div>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Cover
