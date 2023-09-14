a0dc9bd259b3ebb4f8929329dc2901df068d2116
Melvin Mbae <melvinmuthomi95@gmail.com>
2023-09-14 16:12:02 +0300

    Tweak list

--- src/components/CommentField.js
+++ src/components/CommentField.js
@@ -21,21 +21,24 @@ function CommentField(props) {
             })
         }).then(res => res.json())
             .then(comment => {
-                setCommentsDictionary({ ...commentsDictionary, ...{ [album.id]: [...albumComments, comment] } })
+                setCommentsDictionary({ ...commentsDictionary, ...{ [album.id]: [...albumComments, comment] } });
+                setNewComment("");
             })
     }
     return (
-        <>
+        <div className="comments-section">
+            <div className="comments-list">
+                {albumComments.map(comment => (<div className="comment-field">
+                    {comment.user}:<div>{comment.comment}</div>
+                    <button className="comment-btn">Delete</button>
+                    <button className="comment-btn">Edit</button>
+                </div>))}
+            </div>
             <div className="comment-input">
                 Add Comment: <input value={newComment} onChange={(e) => setNewComment(e.target.value)} type="text" placeholder="Add a comment..." />
                 <button onClick={handleSubmit} className="comment-btn">Submit</button>
             </div>
-            {albumComments.map(comment => (<div className="comment-field">
-                {comment.user}:<div>{comment.comment}</div>
-                <button className="comment-btn">Delete</button>
-                <button className="comment-btn">Edit</button>
-            </div>))}
-        </>
+        </div>
     )
 }
 // 
