15319601ed9523377b51e52beca50c57c4ece5b6
Melvin Mbae <melvinmuthomi95@gmail.com>
2023-09-14 18:49:23 +0300

    fetch after submit

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,24 +4,25 @@ import CommentsList from "./CommentsList";
 const commentURL = "http://localhost:3000/comments";
 
 function Comments({ album, commentsDictionary, setCommentsDictionary }) {
-  const [apiComments, setApiComments] = useState([]);
+    const [apiComments, setApiComments] = useState([]);
 
-  function fetchCommentData() {
-    fetch(commentURL)
-      .then((response) => response.json())
-      .then((data) => setApiComments(data));
-  }
+    function fetchCommentData() {
+        fetch(commentURL)
+            .then((response) => response.json())
+            .then((data) => setApiComments(data));
+    }
 
-  useEffect(() => fetchCommentData(), []);
+    useEffect(() => fetchCommentData(), []);
 
-  return (
-    <CommentsList
-      apiComments={apiComments}
-      album={album}
-      commentsDictionary={commentsDictionary}
-      setCommentsDictionary={setCommentsDictionary}
-    />
-  );
+    return (
+        <CommentsList
+            apiComments={apiComments}
+            album={album}
+            commentsDictionary={commentsDictionary}
+            setCommentsDictionary={setCommentsDictionary}
+            fetchCommentData={fetchCommentData}
+        />
+    );
 }
 
 export default Comments;
--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,68 +1,69 @@
 import React, { useState, useEffect } from "react";
-import { createPortal } from "react-dom";
 
 const commentURL = "http://localhost:3000/comments";
 
 function CommentsList(props) {
-  const {
-    album,
-    commentsDictionary,
-    setCommentsDictionary,
-    apiComments = [],
-  } = props;
+    const {
+        album,
+        commentsDictionary,
+        setCommentsDictionary,
+        apiComments = [],
+        fetchCommentData
+    } = props;
 
-  const [newComment, setNewComment] = useState("");
-  const [albumComments, setAlbumComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
+    const [albumComments, setAlbumComments] = useState([]);
 
-  useEffect(
-    () => setAlbumComments(apiComments.filter((x) => x.albumID === album.id)),
-    [apiComments],
-  );
+    useEffect(
+        () => setAlbumComments(apiComments.filter((comment) => comment.albumID === album.id)),
+        [apiComments],
+    );
 
-  function handleSubmit(e) {
-    fetch(commentURL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        user: "Mark Mutugi",
-        comment: newComment,
-        albumID: album.id,
-      }),
-    })
-      .then((res) => res.json())
-      .then((comment) => {
-        setCommentsDictionary({
-          ...commentsDictionary,
-          ...{ [album.id]: [...albumComments, comment] },
-        });
-        setNewComment("");
-      });
-  }
-  return (
-    <div className="comments-section">
-      <div className="comments-list">
-        {albumComments.map((comment) => (
-          <div className="comment-field">
-            {comment.user}:<div>{comment.comment}</div>
-            <button className="comment-btn">Delete</button>
-            <button className="comment-btn">Edit</button>
-          </div>
-        ))}
-      </div>
-      <div className="comment-input">
-        Add Comment:{" "}
-        <input
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          type="text"
-          placeholder="Add a comment..."
-        />
-        <button onClick={handleSubmit} className="comment-btn">
-          Submit
-        </button>
-      </div>
-    </div>
-  );
-}
-//
+    function handleSubmit(e) {
+        fetch(commentURL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                user: "Mark Mutugi",
+                comment: newComment,
+                albumID: album.id,
+            }),
+        })
+            .then((res) => res.json())
+            .then((comment) => {
+                setCommentsDictionary({
+                    ...commentsDictionary,
+                    ...{ [album.id]: [...albumComments, comment] },
+                });
+                fetchCommentData();
+                setNewComment("");
+            });
+    }
+    return (
+        <div className="comments-section">
+            <div className="comments-list">
+                {albumComments.map((comment) => (
+                    <div className="comment-field">
+                        {comment.user}:<div>{comment.comment}</div>
+                        <button className="comment-btn">Delete</button>
+                        <button className="comment-btn">Edit</button>
+                    </div>
+                ))}
+            </div>
+            <div className="comment-input">
+                Add Comment:{" "}
+                <input
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                    type="text"
+                    placeholder="Add a comment..."
+                />
+                <button onClick={handleSubmit} className="comment-btn">
+                    Submit
+                </button>
+            </div>
+        </div>
+    );
+};
+
 export default CommentsList;
